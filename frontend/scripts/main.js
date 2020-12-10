35b4db87616dba4f53b8afe245df72655bbc263b
Glandalf1 <renaud.angles@redraiden.com>
2020-12-10 11:37:00 +0000

    améliore la documentation et le code du front
    Nous avons commenté pas mal de choses pour préciser :
    
    - datasets
    - le comportement des fonctions
    - le fonctionnement du DOM (avec les createElement, etc.)
    
    Nous avons un peu modifié le code pour :
    - lever l'ambiguité entre l'attribut title et les datasets que l'on a utilisé
    - afficher un message d'erreur dans le titre en HTML si notre JS a planté au début de son exécution

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -6,11 +6,18 @@
 const siteTitle = "My website";
 const menu = {
     'Accueil': './index.html',
+    'A propos': './about.html',
 }
 
 start();
 
 function start() {
+    /**
+     * On appelle à la suite ces deux fonctions.
+     * Plus tard, start() fera probablement d'autres choses,
+     * c'est pour cela qu'on a choisi de créer cette fonction
+     * plutôt qu'appeler les deux fonctions directement en dehors.
+     */
     applyTitle();
     displayMenu();
 }
@@ -27,14 +34,21 @@ function displayMenu() {
      */
     document.querySelector('#auto-main-menu').appendChild(document.createElement('ul'));
     for (menuItem in menu) {
-        const domItem = document.createElement('li');
-        domItem.innerText = menuItem;
-        domItem.title = menu[menuItem];
-        domItem.dataset.link = menu[menuItem]; 
+        /**
+         * menuItem a pour valeur à chaque itération la clé actuelle de notre objet `menu`
+         * Par exemple, il vaudra `Accueil` la première fois, puis `A propos` la deuxième
+         * 
+         * menuItem est ce que l'on appelle une `clé`
+         * menu[menuItem] est ce que l'on appelle la "valeur de la clé `menuItem`"
+         */ 
+        const domItem = document.createElement('li');       // <li></li>
+        domItem.innerText = menuItem;                       // <li>Accueil</li>
+        domItem.title = 'Découvrir la page ' + menuItem;    // <li title="Découvrir la page Accueil">Accueil</li>
+        domItem.dataset.link = menu[menuItem];              // <li title="Découvrir la page Accueil" data-link="./index.html">Accueil</li>
 
         domItem.onclick = function(ev) {
-            window.location.href = ev.target.dataset.link;
-        } 
+            window.location = ev.target.dataset.link;
+        }                                                   // <li title="Découvrir la page Accueil" data-link="./index.html" onclick="..."">Accueil</li>
         document.querySelector('#auto-main-menu ul').appendChild(domItem);
     }
 }
